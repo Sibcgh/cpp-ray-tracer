1f706fa18812c7537480a6b139ffa6cb1a519b36
Sibghat.Shah <sibghat.shah01@gmail.com>
2024-10-30 20:21:57 -0400

    init

---
+++ build/CMakeFiles/CppRayTracer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppRayTracer.dir/src/main.cc.o"
+  "CMakeFiles/CppRayTracer.dir/src/main.cc.o.d"
+  "CppRayTracer"
+  "CppRayTracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppRayTracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/CppRayTracer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CppRayTracer.
+# This may be replaced when dependencies are built.

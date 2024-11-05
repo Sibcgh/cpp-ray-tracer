b0e5655f6f74d4bc4c8a249e58f9265b275d7e32
Sibghat.Shah <sibghat.shah01@gmail.com>
2024-11-05 17:21:05 -0500

    hitting a trivial sphere

--- src/main.cc
+++ src/main.cc
@@ -4,12 +4,34 @@
 
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
 
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0,0,-1), 0.5, r))
+        return color(1, 0, 0);
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 }
+
+
+// color ray_color(const ray& r) {
+//     vec3 unit_direction = unit_vector(r.direction());
+//     auto a = 0.5*(unit_direction.y() + 1.0);
+//     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+//     /*
+//     while is [1, 1, 1]
+//     light blue is [0.5, 0.7, 1]
+//     */
+// }
 
 int main() {
 
@@ -41,6 +63,10 @@ int main() {
     auto viewport_upper_left = camera_center
                              - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    // Print the value of viewport_upper_left without affecting PPM output
+    // std::clog << "Viewport Upper Left: " << viewport_upper_left << std::endl;
+
 
     // Render
 

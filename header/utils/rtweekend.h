46d3a3e8ce024c9924cea3eae769652b5f0dbd79
Sibghat.Shah <sibghat.shah01@gmail.com>
2024-11-10 21:44:29 -0500

    added gamma correction

--- header/utils/rtweekend.h
+++ header/utils/rtweekend.h
@@ -20,11 +20,13 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
-    // Random Number generator
 inline double random_double() {
-    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
-    static std::mt19937 generator;
-    return distribution(generator);
+    return rand() / (RAND_MAX + 1.0);
+}
+
+// Overload to handle min and max range
+inline double random_double(double min, double max) {
+    return min + (max - min) * random_double();
 }
 
 // Common Headers
